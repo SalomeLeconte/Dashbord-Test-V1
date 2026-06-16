8ac2c9fde26f677b6b1ec7d5140dea19cc7c4b67
Salomé Leconte <104072745+SalomeLeconte@users.noreply.github.com>
2026-06-16 06:47:10 +0200

    Add React helper module

---
+++ src/lib/react.js
@@ -0,0 +1,10 @@
+export const React = window.React;
+export const h = React.createElement;
+export const {
+    Fragment,
+    useCallback,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+} = React;
