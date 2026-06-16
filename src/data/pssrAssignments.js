5fb1d1754a8910dec68e73c780d4d5e7e8066320
Salomé Leconte <104072745+SalomeLeconte@users.noreply.github.com>
2026-06-16 06:47:21 +0200

    Add PSSR department assignments

---
+++ src/data/pssrAssignments.js
@@ -0,0 +1,11 @@
+export const pssrAssignments = [
+    { name: 'CHRISTOPHER BORRHOMEE', departments: ['60', '75', '77', '78', '91', '92', '93', '94', '95'], machine: 'excavator' },
+    { name: 'STEPHANE ASCLAR', departments: ['77', '21', '52', '71', '25', '39', '70', '90', '89'], machine: 'dozer' },
+    { name: 'DAVID DEVILLIERS', departments: ['18', '28', '36', '37', '41', '45'], machine: 'loader' },
+    { name: 'DENIS RENAULT', departments: ['1', '7', '26', '42', '69'], machine: 'excavator' },
+    { name: 'JULIEN POIROT', departments: ['38', '73', '74'], machine: 'dozer' },
+    { name: 'LUDOVIC GRAC', departments: ['4', '5', '13', '84'], machine: 'loader' },
+    { name: 'JEREMY CHEDOTAL', departments: ['9', '12', '31', '32', '46', '81', '82', '11', '30', '34', '66'], machine: 'excavator' },
+    { name: 'LIONEL VALE', departments: ['3', '15', '43', '48', '58', '63'], machine: 'dozer' },
+    { name: 'MELODY DEMEYERE', departments: ['19', '23', '87'], machine: 'loader' },
+];
