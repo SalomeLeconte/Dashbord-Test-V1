30cad6ef53144f593a25f6cb5241750c04a9ca9c
Salomé Leconte <104072745+SalomeLeconte@users.noreply.github.com>
2026-08-10 08:22:07 +0200

    Copy canton note cleanup patch in build

--- scripts/build-cloudflare.mjs
+++ scripts/build-cloudflare.mjs
@@ -50,6 +50,7 @@ copyFileIfExists('wip-siret-dedupe-patch.js');
 copyFileIfExists('wip-undercarriage-detail-ui-patch.js');
 copyFileIfExists('wip-release-target-precision-patch.js');
 copyFileIfExists('wip-undercarriage-modal-clean-patch.js');
+copyFileIfExists('wip-canton-note-cleanup-patch.js');
 
 // Cloudflare Pages configuration files. They must be present in the output directory.
 copyFileIfExists('_headers');
